54b4bb4d844fa46d38df3a848c080a0b6fa92927
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-03-27 13:05:45 +0700

    Pertemuan5_Percobaan2

--- AlgoritmaStrukturData/src/Pertemuan4/BruteForceDivideConquer/minggu5/Faktorial.java
+++ AlgoritmaStrukturData/src/Pertemuan4/BruteForceDivideConquer/minggu5/Faktorial.java
@@ -21,5 +21,7 @@ public class Faktorial {
             return fakto;
         }
     }
+
+ 
 }
 
---
+++ AlgoritmaStrukturData/src/Pertemuan4/BruteForceDivideConquer/minggu5/MainPangkat.java
@@ -0,0 +1,40 @@
+package Pertemuan4.BruteForceDivideConquer.minggu5;
+
+import java.util.Scanner;
+
+public class MainPangkat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==========================================");
+        System.out.print("Masukkan jumlah elemen yang ingin dihitung: ");
+        int elemen = sc.nextInt();
+
+        Pangkat[] png = new Pangkat[elemen];
+        for (int i = 0; i < elemen; i++) {
+            png[i] = new Pangkat();
+            System.out.print("Masukkan nilai yang hendak dipangkatkan: ");
+            int nilai = sc.nextInt();
+            png[i].nilai = nilai;
+            System.out.print("Masukkkan nilai pemangkat: ");
+            int pngkat = sc.nextInt();
+            png[i].pangkat = pngkat;
+        }
+
+        System.out.println("HASIL PANGKAT - BRUTE FORCE");
+        for (int i = 0; i < elemen; i++) 
+        System.out.println
+        ("Hasil Dari: "
+        +png[i].nilai+" pangkat "
+        +png[i].pangkat+" adalah "
+        +png[i].pangkatBF(png[i].nilai, png[i].pangkat));{    
+        }
+        System.out.println("HASIL PANGKAT - DIVIDE CONQUER");
+        for (int i = 0; i < elemen; i++) 
+        System.out.println
+        ("Hasil Dari: "
+        +png[i].nilai+" pangkat "
+        +png[i].pangkat+" adalah "
+        +png[i].pangkatDC(png[i].nilai, png[i].pangkat));{    
+        }
+    }
+}
