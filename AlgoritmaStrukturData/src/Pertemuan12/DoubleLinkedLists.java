783bac3625e1725d05af1942bd07fc94c5b44e44
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-05-14 09:16:25 +0700

    Pertemuan11_Percobaan2_16

---
+++ AlgoritmaStrukturData/src/Pertemuan12/DoubleLinkedLists.java
@@ -0,0 +1,39 @@
+package Pertemuan12;
+
+public class DoubleLinkedLists {
+    Node head;
+    int size;
+
+    DoubleLinkedLists(){
+
+    }
+
+    Boolean isEmpty(){
+        return true;
+    }
+
+    void addFirst(){
+
+    }
+
+    void addLast(){
+
+    }
+
+    void add (int item, int index){
+
+    }
+
+    int size(){
+        return 1;
+    }
+
+    void clear(){
+
+    }
+
+    void print(){
+        
+    }
+
+}
---
+++ AlgoritmaStrukturData/src/Pertemuan12/Node.java
@@ -0,0 +1,15 @@
+package Pertemuan12;
+
+public class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+
+    Node (Node prev, int data, Node next){
+
+    }
+    
+    Node (){
+
+    }
+}
