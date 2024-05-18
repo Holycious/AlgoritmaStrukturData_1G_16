e72f0b6f32179c8ee5fc2e7b96882d64b85b8775
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-05-18 18:00:06 +0700

    Pertemuan12_Percobaan1_16

---
+++ AlgoritmaStrukturData/src/Pertemuan12/doublelinkedlists/DoubleLinkedListsMain.java
@@ -0,0 +1,25 @@
+package Pertemuan12.doublelinkedlists;
+
+public class DoubleLinkedListsMain {
+    public static void main(String[] args) throws Exception {
+        Doublelinkedlists dll = new Doublelinkedlists();
+        dll.print();
+        System.out.println("Size : "+dll.size);
+        System.out.println("=====================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : "+dll.size);
+        System.out.println("=====================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : "+dll.size);
+        System.out.println("=====================================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : "+dll.size);
+        System.out.println("=====================================");
+        
+    }
+}   
