a4cf25d91c0e476ce2846c4676a08f0ace45523c
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-06-09 11:17:15 +0700

    Pertemuan14_Percobaan2_16

--- AlgoritmaStrukturData/src/Pertemuan14/GraphMain16.java
+++ AlgoritmaStrukturData/src/Pertemuan14/GraphMain16.java
@@ -4,32 +4,43 @@ import java.util.Scanner;
 
 public class GraphMain16 {
     public static void main(String[] args) throws Exception{
-        Graph16 gedung = new Graph16(6);
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        // gedung.degree(0);
-        // gedung.printGraph();
-        // gedung.removeEdge(1, 3);
-        // gedung.printGraph();
-        Scanner leon = new Scanner(System.in);
-        System.out.print("Masukkan asal gedung: ");
-        int asal = leon.nextInt();
-        System.out.print("Masukkan gedung tujuan: ");
-        int tujuan = leon.nextInt();
+        GraphMatriks16 gdg = new GraphMatriks16(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan Edge:");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+        // Graph16 gedung = new Graph16(6);
+        // gedung.addEdge(0, 1, 50);
+        // gedung.addEdge(0, 2, 100);
+        // gedung.addEdge(1, 3, 70);
+        // gedung.addEdge(2, 3, 40);
+        // gedung.addEdge(3, 4, 60);
+        // gedung.addEdge(4, 5, 80);
+        // // gedung.degree(0);
+        // // gedung.printGraph();
+        // // gedung.removeEdge(1, 3);
+        // // gedung.printGraph();
+        // Scanner leon = new Scanner(System.in);
+        // System.out.print("Masukkan asal gedung: ");
+        // int asal = leon.nextInt();
+        // System.out.print("Masukkan gedung tujuan: ");
+        // int tujuan = leon.nextInt();
 
-        if (asal < 0 || asal > 5 || tujuan < 0 || tujuan > 5) {
-            System.out.println("Gedung yang dimasukkan tidak valid");
-        } else {
-            if (gedung.adjacency(asal,tujuan)) {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
-            } else {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
-            }
-        }
+        // if (asal < 0 || asal > 5 || tujuan < 0 || tujuan > 5) {
+        //     System.out.println("Gedung yang dimasukkan tidak valid");
+        // } else {
+        //     if (gedung.adjacency(asal,tujuan)) {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
+        //     } else {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
+        //     }
+        // }
         
         
     }
---
+++ AlgoritmaStrukturData/src/Pertemuan14/GraphMatriks16.java
@@ -0,0 +1,55 @@
+package Pertemuan14;
+
+public class GraphMatriks16 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks16(int v){
+        vertex = v;
+        matriks = new int[v][v];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak){
+        matriks[asal][tujuan] = jarak;
+    }
+
+    public void removeEdge(int asal, int tujuan){
+        matriks[asal][tujuan] = 0;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + ": ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != 1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + "m), ");
+ 
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public int OutDegree(int gedung) {
+        int OutDegree = 0;
+        for (int i = 0; i < vertex; i++) {
+            if (matriks[gedung][i] != 0) {
+                OutDegree++;
+            }
+        }
+        return OutDegree;
+    }
+    
+    public int InDegree(int gedung) {
+        int InDegree = 0;
+        for (int i = 0; i < vertex; i++) {
+            if (matriks[i][gedung] != 0) {
+                InDegree++;
+            }
+        }
+        return InDegree;
+    }
+
+    public int Degree(int gedung){
+        return InDegree(gedung) + OutDegree(gedung);
+    }
+}
